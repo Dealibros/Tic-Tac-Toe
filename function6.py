8d8043e3b4fca720e1db9781a5c4b47721bd93d7
Dealibros <dealibros88@gmail.com>
2022-03-29 13:21:54 +0200

    sorted einning function issue

--- function6.py
+++ function6.py
@@ -2,34 +2,34 @@ def get_winning_player(board):
     # Horizontal rown check
     for row in board:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] != ["."]:
+        if row.count(row[0]) == len(row) and row[0] != ".":
             print(f"Player {row[0]} is the Winner horizontal!")
             return row[0]
   
-    # For vertical row check
+    # For vertical row check!= ["."]
     for col in range(len(board)):
         check = []
 
     for row in board:
         check.append(row[col])
+        print("check", check)
 
-        if check.count(check[0]) == len(check) and check[0] != ["."]:
+        if check.count(check[0]) == len(check) and check[0] != ".":
             print(f"Player {check[0]} is the Winner vertical!")
             return check[0]
 
     # for diagonal row check
     diags = []
     for col, row in enumerate(reversed(range(len(board)))):
-        print(col, row)
         diags.append(board[row][col])
-    if diags.count(row[0]) == len(row) and row[0] != ["."]:
+    if diags.count(diags[0]) == len(diags) and diags[0] != ".":
         print(f"Player {diags[0]} is the Winner diagonal!")
         return diags[0]
 
     diags = []
     for index in range(len(board)):
         diags.append(board[index][index])
-    if diags.count(row[0]) == len(row) and row[0] != ["."]:
+    if diags.count(diags[0]) == len(diags) and diags[0] != ".":
         print(f"Player {diags[0]} is the Winner diagonal2!")
         return diags[0]
 
