3974dbe53e71f09c67481c802fee4f1abf4b81dc
Dealibros <dealibros88@gmail.com>
2022-03-30 18:18:58 +0200

    cleaning up code

--- function4.py
+++ function4.py
@@ -1,39 +1,46 @@
 import random
 
+
+
+
 def get_random_ai_coordinates(board, current_player):
-  """
-  Should return a tuple of 2 numbers. 
-  Each number should be between 0-2. 
-  The chosen number should be only a free coordinate from the board.
-  If the board is full (all spots taken by either X or O) than "None"
-  should be returned.
-  """
+    random_choice = True
+    while random_choice: 
+        coordinates = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+        a, b = coordinates.pop(random.randrange(len(coordinates)))
+        print(a, b)
+        if board[a][b] != "X" and board[a][b] != "O":
+            board[a][b] = current_player
+            random_choice = True
+            return board[a][b]   
+        else:
+            random_choice = False
+            print(a, b)
+            print("cydd")
+            break
 
-  """def get_coord():
-      return (1, 1)
+
+    """
+    Should return a tuple of 2 numbers. 
+    Each number should be between 0-2. 
+    The chosen number should be only a free coordinate from the board.
+    If the board is full (all spots taken by either X or O) than "None"
+    should be returned.
+    """
+
+    """def get_coord():
+        return (1, 1)
 
 
-  ai_move = get_coord()
-  board[ai_move[0]][ai_move[1]] = 'X'
-  print(board)
+    ai_move = get_coord()
+    board[ai_move[0]][ai_move[1]] = 'X'
+    print(board)
 
-  x_coord, y_coord = get_coord()
-  board[x_coord][y_coord] = 'O'
-  print(board)"""
+    x_coord, y_coord = get_coord()
+    board[x_coord][y_coord] = 'O'
+    print(board)"""
 
-  random_choose = True
-  while random_choose:
-    coordinates = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
-    a,b = coordinates.pop(random.randrange(len(coordinates)))
-    print("print new list",coordinates)
-    if board[a][b] != "X" and board[a][b] != "O":
-      random_choose = True
-      return a,b
-      
-    else:
-      random_choose = False
-      print("cydd")
-      break
+     
       #board[choose_random_coordinate[0],choose_random_coordinate[1]] = current_player
       
 
@@ -51,9 +58,9 @@ if __name__ == "__main__":
   print(get_random_ai_coordinates(board_1, "X")) # the printed coordinate should be only (0,2) or (1,2)
  """
   
-  board_2 = [
+  """ board_2 = [
     ["O", "X", "X"],
     ["X", "O", "X"],
     ["X", "O", "X"],
   ]
-  print(get_random_ai_coordinates(board_2, "O")) # the printed coordinate should be None
+  print(get_random_ai_coordinates(board_2, "O")) """ # the printed coordinate should be None
--- trial_display_board.py
+++ trial_display_board.py
@@ -23,3 +23,44 @@ def display_board(board):
       ['.', ".", "."]
    ] """
    
+
+
+  print(board)
+  # The board is a list of lists.
+  coordinates = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)] 
+  a, b, c, d, e, f, g, h, i = coordinates
+  print(a)
+  # coordinates = [board[0][0], board[0][1], board[0][2], board[1][0], board[1][1], board[1][2], board[2][0], board[2][1], board[2][2]]
+  # choose_random_coordinate = random.choice(coordinates)
+  choose_random_coordinate = coordinates.pop(random.randrange(len(coordinates)))
+  # print(choose_random_coordinate.index(choose_random_coordinate))
+  correct_format_coordinate = board[int(choose_random_coordinate[0]), int(choose_random_coordinate[1])]
+  print("correct?", correct_format_coordinate)
+  
+  # pop will remove the chosen coordinate from the list
+  # now the coordinate choosen by the other player must be remove as well
+  
+  print("coordinates left", coordinates)
+
+
+  import random
+
+
+def get_random_ai_coordinates(board, current_player):
+    random_choose = True
+    while random_choose:
+        coordinates = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+        randomcoord = coordinates.pop(random.randrange(len(coordinates)))
+        print("randomcoord", randomcoord)
+        a, b = randomcoord
+        print(a, b)
+        print("print new list", coordinates)
+        if board[a][b] != "X" and board[a][b] != "O":
+            random_choose = True
+            return a, b
+          
+        else:
+            random_choose = False
+            print("This coordinate is already taken")
+            
+
